60bf3f6a831c0d1a38aefee9a6c21aff03f071d6
Chechu <sergio.abad.ferreria@gmail.com>
2020-05-19 19:10:02 +0200

    posible fallo en el classpath

--- src/conn/Connexio.java
+++ src/conn/Connexio.java
@@ -9,6 +9,7 @@ package conn;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -21,7 +22,7 @@ public class Connexio {
  
 		try {
                         Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://79.137.83.198:3306/ferreria?user=ferreria&password=Ferreria29!";
+			String url = "jdbc:mysql://79.137.83.198:3306/sakila?user=ferreria&password=Ferreria29!";
 			con = DriverManager.getConnection(url);
 			if (con != null) {
 				System.out.println("Conexió ok");
@@ -32,4 +33,8 @@ public class Connexio {
 		}
 		return con;
 	}
+
+    public Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- src/informes/FXMLDocumentController.java
+++ src/informes/FXMLDocumentController.java
@@ -5,31 +5,95 @@
  */
 package informes;
 
+import conn.Connexio;
+import entidades.Pelicula;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.export.Exporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  *
  * @author rvallez
  */
 public class FXMLDocumentController implements Initializable {
+
+    private LinkedList<Pelicula> peliculas = new LinkedList<>();
+    private final String query = "SELECT f.film_id, f.title, f.release_year, f.rating, l.name FROM film f, language l WHERE f.language_id = l.language_id limit 50;";
+    private JasperPrint reportToPrint;
     
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        try {
+            Connexio conexio = new Connexio();
+            Connection con = conexio.conectar();
+            Statement st = con.createStatement();
+            ResultSet result = st.executeQuery(query);
+
+            while (result.next()) {
+                peliculas.add(new Pelicula(result.getInt(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5)));
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+
+        try {
+          
+            JasperCompileManager.compileReport("src/informes/report.jrxml");
+        } catch (JRException ex) {
+            System.out.println(ex);
+        }
+
+        try {
+            reportToPrint = JasperFillManager.fillReport("src/informes/report.jasper", null, new JRBeanCollectionDataSource(peliculas));
+        } catch (JRException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @FXML
+    public void toPDF() throws JRException {
+        Exporter exporter = new JRPdfExporter();
+        exporter.setExporterInput(new SimpleExporterInput(reportToPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput("report.pdf"));
+        exporter.exportReport();
+    }
+
+    @FXML
+    public void toDOC() throws JRException {
+
+        Exporter exporter = new JRDocxExporter();
+        exporter.setExporterInput(new SimpleExporterInput(reportToPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput("report.docx"));
+        exporter.exportReport();
+    }
+
+    @FXML
+    public void informeMostrar() throws JRException {
+        JasperViewer jasperViewer = new JasperViewer(reportToPrint);
+        jasperViewer.setVisible(true);
+    }
+
 }
